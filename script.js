03e37db143bb642062e0045088e12ace1e835299
and-roque <amlroque@gmail.com>
2022-07-19 17:37:24 +0100

    Create function for set table size

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container");
+
+function setTable (n) {
+    for (let i=0; i<n; i++){
+        const newColumn = document.createElement("div");
+        newColumn.classList.add(`cl${i}`);
+        newColumn.classList.add(`column`);
+        container.appendChild(newColumn);
+        for (let x=0; x<n; x++){
+            const column = document.querySelector(`.cl${i}`);
+            const newLine = document.createElement("div");
+            newLine.classList.add('line');
+            column.appendChild(newLine);
+        }
+    }
+}
+
+setTable(16);
+    
+
+
