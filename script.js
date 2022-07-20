673cda11a948c8001fe42fdd8eace7f1f7590df6
and-roque <amlroque@gmail.com>
2022-07-20 16:49:25 +0100

    Add on/off painting whith mouseclick

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector(".container");
 let tableSize =16;
+let mode='black';
+let click = false;
 setTable(tableSize);
 drawing();
 //drawing("black");
-let mode='black';
+
 
 //Set table size
 function setTable (n) {
@@ -40,28 +42,31 @@ btnSetTableSize.addEventListener('click', () => {
 
 //Event listener for "Hover -> change background"
 function drawing (){
-    const squares = document.querySelectorAll('.square');
-    squares.forEach((square)=>{
-        square.addEventListener('mouseover', ()=>{
-            if(mode === "randomRgb"){
-                square.style.backgroundColor = randomRgb(); 
-            } else if (mode === "black"){
-                square.style.backgroundColor = "black";
-                
-            } else if (mode === "erase"){
-                square.style.backgroundColor = "white";
-            } else {
-                let currentOpacity = Number(square.style.backgroundColor.slice(-4, -1));
-                if (currentOpacity<1){
-                    square.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
-                } else if (currentOpacity=1){
-                    return;
-                } else {
-                    square.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+    
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square)=>{
+            square.addEventListener('mouseover', ()=>{
+                if (click){
+                    if(mode === "randomRgb"){
+                        square.style.backgroundColor = randomRgb(); 
+                    } else if (mode === "black"){
+                        square.style.backgroundColor = "black";
+                        
+                    } else if (mode === "erase"){
+                        square.style.backgroundColor = "white";
+                    } else {
+                        let currentOpacity = Number(square.style.backgroundColor.slice(-4, -1));
+                        if (currentOpacity<1){
+                            square.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+                        } else if (currentOpacity=1){
+                            return;
+                        } else {
+                            square.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+                        }
+                    }
                 }
-            }
+            });
         });
-    });
 };
 
 
@@ -72,13 +77,23 @@ function randomRgb () {
     return value;
 };
 
-
+//Set Painting Mode
 const colorOptions = document.querySelectorAll('.colorOptions');
 colorOptions.forEach((option)=>{
         option.addEventListener('click',()=>{
             if (option.id=== "clean"){
+                if(mode="erase") mode = "black";
                 setTable(tableSize);
                 drawing();
             } else mode = option.id;   
         })
     });
+
+
+//Click ON/OFF painting
+container.onmousedown = () => {
+    click = true;
+};
+container.onmouseup = () => {
+    click = false;
+};
