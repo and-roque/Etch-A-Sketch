e8ab52d3314800a1f5bf7157cda45d27996280e7
and-roque <amlroque@gmail.com>
2022-07-20 16:00:37 +0100

    Add different modes of painting

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
 setTable(16);
 drawing();
+//drawing("black");
+let mode;
 
 //Set table size
 function setTable (n) {
@@ -29,25 +31,46 @@ btnSetTableSize.addEventListener('click', () => {
     if (tableSize>100 || tableSize<1){
         tableSize = prompt("Please set a size between 1-100");
     } else {
-        setTable(tableSize);
-        console.log("HEY");
+        setTable(tableSize); 
         drawing();
     }
 });
 
 //Event listener for "Hover -> change background"
-function drawing() {
+function drawing (){
     const squares = document.querySelectorAll('.square');
     squares.forEach((square)=>{
-    square.addEventListener('mouseover', ()=>{
-        //square.style.backgroundColor = "black";
-        square.style.backgroundColor = randomRgb();
+        square.addEventListener('mouseover', ()=>{
+            if(mode === "randomRgb"){
+                square.style.backgroundColor = randomRgb(); 
+            } else if (mode === "black"){
+                square.style.backgroundColor = "black";
+            } else {
+                let currentOpacity = Number(square.style.backgroundColor.slice(-4, -1));
+                if (currentOpacity<1){
+                    square.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+                } else if (currentOpacity=1){
+                    return;
+                } else {
+                    square.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+                }
+            }
         });
     });
 };
 
+
+//Random RGB generator
 function randomRgb () {
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     let value = "#"+ randomColor;
     return value;
 };
+
+
+const colorOptions = document.querySelectorAll('.colorOptions');
+colorOptions.forEach((option)=>{
+        option.addEventListener('click',()=>{
+            mode = option.id;   
+        })
+    });
